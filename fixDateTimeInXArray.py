01aa9cc3bc094cd933fbfe3a3eb2c068ac710d03
naomi <naomi@universesandbox.com>
2017-01-16 09:49:54 -0800

    Added header imports, and now closes files to avoid error of too many files open.

--- fixDateTimeInXArray.py
+++ fixDateTimeInXArray.py
@@ -1,3 +1,11 @@
+import numpy as np
+import scipy as sp
+import xarray as xy
+import pandas as pd
+import string
+import os.path
+from copy import deepcopy
+
 directoryData = ./ #put the directory path here
 field = 'A_PCP_110_SFC_acc'
     
@@ -16,10 +24,10 @@ def getOneDay(oneFile):
     time = pd.datetime(year,1,1) + pd.DateOffset(days=dayOfYear-1) 
     time = [time]
     prData = xy.open_dataset(oneFile,decode_cf=False) 
-    lon = prData['lon_110']
-    lat = prData['lat_110']
-    prData = prData[field]
-    prOneDay = xy.DataArray(prData, coords=[time,lat,lon],dims=['time','lat','lon'])
+    lon = deepcopy(prData['lon_110'])
+    lat = deepcopy(prData['lat_110'])
+    prOneDay = xy.DataArray(deepcopy(prData[field]), coords=[time,lat,lon],dims=['time','lat','lon'])
+    prData.close()
     return prOneDay
 
 pr = xy.concat([getOneDay(oneFile) for oneFile in fileList], dim='time')
